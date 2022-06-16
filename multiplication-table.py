d5e0c463fc1625db7e21c7bad424f7b0bb93c8b3
Opu Hasnat <opqclick@gmail.com>
2022-06-16 19:43:25 +0600

    f expression

--- multiplication-table.py
+++ multiplication-table.py
@@ -3,5 +3,6 @@ from tkinter.tix import MAX
 
 num = int(input("What multiplication table you want? "))
 mx = [(x+1) * num for x in range(20)]
-
-print(mx)
+mx2 = [str(x+1)+ ' x ' + str(num)+' = ' +str((x+1)*num) for x in range(20)]
+mx3 = [f'{x+1} x {num} = {(x+1)*num}' for x in range(20)]
+print(mx3)
---
+++ watermelon.py
@@ -0,0 +1,5 @@
+user_input_N = int(input("How many watermelons you want to purchase? "))
+watermelon_initial_unit_price_A = 0
+
+
+
